208d6e5df3d00f5850e3177211b5a2777e7e0ca3
gabrielbazante <gabrielbazante7@gmail.com>
2023-04-20 16:47:22 -0300

    Product registration service

--- produtos/src/main/java/br/com/api/products/service/ProductService.java
+++ produtos/src/main/java/br/com/api/products/service/ProductService.java
@@ -1,9 +1,12 @@
 package br.com.api.products.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.com.api.products.model.ProductModel;
+import br.com.api.products.model.ResponseModel;
 import br.com.api.products.repository.ProductRepository;
 
 @Service
@@ -12,7 +15,22 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private ResponseModel responseModel;
+
     public Iterable<ProductModel> listAllProducts() {
         return productRepository.findAll();
+    }
+
+    public ResponseEntity<?> saveProduct(ProductModel productModel){
+        if(productModel.getName().equals("")) {
+            responseModel.setMessage("Product name cannot be empty");
+            return new ResponseEntity<ResponseModel>(responseModel, HttpStatus.BAD_REQUEST);
+        } else if (productModel.getBrand().equals("")) {
+            responseModel.setMessage("Product brand cannot be empty");
+            return new ResponseEntity<ResponseModel>(responseModel, HttpStatus.BAD_REQUEST);
+        } else {
+            return new ResponseEntity<ProductModel>(productRepository.save(productModel), HttpStatus.CREATED);
+        }
     }
 }
