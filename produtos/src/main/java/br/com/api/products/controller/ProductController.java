962db3d3b6698bb9683c10ff57ea5fe6c78aeb3d
gabrielbazante <gabrielbazante7@gmail.com>
2023-04-20 16:59:14 -0300

    Creating the route to change a product

--- produtos/src/main/java/br/com/api/products/controller/ProductController.java
+++ produtos/src/main/java/br/com/api/products/controller/ProductController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,10 @@ public class ProductController {
     @PostMapping("/registerProduct")
     public ResponseEntity<?> saveProduct(@RequestBody ProductModel productModel) {
         return productService.saveProduct(productModel, "create");
+    }
+
+    @PutMapping("/updateProduct")
+    public ResponseEntity<?> updateProduct(@RequestBody ProductModel productModel) {
+        return productService.saveProduct(productModel, "update");
     }
 }
