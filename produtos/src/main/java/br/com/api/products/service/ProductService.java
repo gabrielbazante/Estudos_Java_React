fddbeb0556503bb3d779961b335d19685278dc4f
gabrielbazante <gabrielbazante7@gmail.com>
2023-04-20 16:37:21 -0300

    Creation of the method of listing all products

--- produtos/src/main/java/br/com/api/products/service/ProductService.java
+++ produtos/src/main/java/br/com/api/products/service/ProductService.java
@@ -1,5 +1,18 @@
 package br.com.api.products.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.api.products.model.ProductModel;
+import br.com.api.products.repository.ProductRepository;
+
+@Service
 public class ProductService {
     
+    @Autowired
+    private ProductRepository productRepository;
+
+    public Iterable<ProductModel> listAllProducts() {
+        return productRepository.findAll();
+    }
 }
