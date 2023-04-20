2f28fee26beb250c918a6d1aa98b0a65fb759526
gabrielbazante <gabrielbazante7@gmail.com>
2023-04-20 16:39:56 -0300

    Creating a route to list all products

--- produtos/src/main/java/br/com/api/products/controller/ProductController.java
+++ produtos/src/main/java/br/com/api/products/controller/ProductController.java
@@ -1,13 +1,20 @@
 package br.com.api.products.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.api.products.model.ProductModel;
+import br.com.api.products.service.ProductService;
+
 @RestController
 public class ProductController {
+
+    @Autowired
+    private ProductService productService;
     
-    @GetMapping("/")
-    public String routeTest() {
-        return "Products API is running!";
+    @GetMapping("/listAllProducts")
+    public Iterable<ProductModel> listAllProducts() {
+        return productService.listAllProducts();
     }
 }
