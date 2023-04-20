66f943dbd49dd0f1f540a63b373af5332c195a32
gabrielbazante <gabrielbazante7@gmail.com>
2023-04-20 16:31:09 -0300

    Creation of a test route to verify that everything was working correctly.

--- produtos/src/main/java/br/com/api/products/controller/ProductController.java
+++ produtos/src/main/java/br/com/api/products/controller/ProductController.java
@@ -1,5 +1,13 @@
 package br.com.api.products.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProductController {
     
+    @GetMapping("/")
+    public String routeTest() {
+        return "Products API is running!";
+    }
 }
