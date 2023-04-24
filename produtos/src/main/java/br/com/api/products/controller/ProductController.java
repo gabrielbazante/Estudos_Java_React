951db01f78567202f8c49037d9db226d668787d2
gabrielbazante <gabrielbazante7@gmail.com>
2023-04-24 15:47:12 -0300

    Added note for issues with CORS

--- produtos/src/main/java/br/com/api/products/controller/ProductController.java
+++ produtos/src/main/java/br/com/api/products/controller/ProductController.java
@@ -2,6 +2,7 @@ package br.com.api.products.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import br.com.api.products.model.ResponseModel;
 import br.com.api.products.service.ProductService;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ProductController {
 
     @Autowired
