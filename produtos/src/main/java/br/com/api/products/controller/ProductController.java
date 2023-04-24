8fdd259fd35c3e294defe718a6b6a1ebc015bf4a
gabrielbazante <gabrielbazante7@gmail.com>
2023-04-24 15:42:44 -0300

    Creating a route to delete a product

--- produtos/src/main/java/br/com/api/products/controller/ProductController.java
+++ produtos/src/main/java/br/com/api/products/controller/ProductController.java
@@ -2,13 +2,16 @@ package br.com.api.products.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.api.products.model.ProductModel;
+import br.com.api.products.model.ResponseModel;
 import br.com.api.products.service.ProductService;
 
 @RestController
@@ -30,5 +33,10 @@ public class ProductController {
     @PutMapping("/updateProduct")
     public ResponseEntity<?> updateProduct(@RequestBody ProductModel productModel) {
         return productService.saveProduct(productModel, "update");
+    }
+
+    @DeleteMapping("/deleteProduct/{id}")
+    public ResponseEntity<ResponseModel> deleteProduct(@PathVariable long id) {
+        return productService.deleteProduct(id);
     }
 }
